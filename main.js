8379b2dbce8c02f627fc62de6174110e0c8017bd
Stefano <stefano2890@gmail.com>
2020-04-20 17:24:00 +0200

    Inserito Math.rounded

--- main.js
+++ main.js
@@ -10,10 +10,13 @@ var sconto20 = kilometer * (prezzo_km - ((20 / 100) * prezzo_km));
 var sconto40 = kilometer * (prezzo_km - ((40 / 100) * prezzo_km));
 // prezzo Biglietto intero
 var intero = kilometer * prezzo_km
+// se è minore di 18 anni sconto20, inserito il .toFixed per stampare solo 2 cifre dopo la virgola e Math.rounded per arrotondare
 if (eta < 18){
-    console.log('Prezzo del biglietto per minorenne: € ' + sconto20.toFixed(2));
+    console.log('Prezzo del biglietto per minorenne: € ' + (Math.round(sconto20 * 100)/100).toFixed(2));
+    // altrimenti se ha 65 o più anni sconto40
 } else if (eta >= 65){
-    console.log('Prezzo del biglietto per Over 65: € ' + sconto40.toFixed(2));
+    console.log('Prezzo del biglietto per Over 65: € ' + (Math.round(sconto40 * 100)/100).toFixed(2));
+    // altrimenti prezzo intero tra i 18 e i 64 anni
 } else {
-    console.log('Prezzo del biglietto intero: € ' + intero.toFixed(2));
+    console.log('Prezzo del biglietto intero: € ' + (Math.round(intero * 100)/100).toFixed(2));
 }
